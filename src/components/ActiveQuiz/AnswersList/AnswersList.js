3dd301247a02a8f1d7a583b8ba46d720b2cdc74e
Jana Savchenko <yana.s.savchenko@gmail.com>
2020-04-10 13:56:46 +0300

    Add answers list

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './AnswersList.module.css';
+import AnswerItem from './AnswerItem/AnswerItem'
+
+const AnswersList = props => (
+
+  <ul className={classes.answersList}>
+    { props.answers.map((answer, index) => {
+      return (
+        <AnswerItem 
+          key={index}
+          answer={answer} 
+        />
+      )
+    })}
+  </ul>
+
+)
+
+export default AnswersList;
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,15 +6,26 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 class Quiz extends React.Component {
 
   state = {
-    quiz: [],
+    quiz: [
+      {
+        answers: [
+          {text: 'Question 1'},
+          {text: 'Question 2'},
+          {text: 'Question 3'},
+          {text: 'Question 4'},
+        ]
+      }
+    ],
   }
 
   render() {
     return (
       <div className={classes.quiz}>
         <div className={classes.quizWrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz />
+          <h1>Quiz name</h1>
+          <ActiveQuiz 
+            answers={this.state.quiz[0].answers}
+          />
         </div>
       </div>
     )
