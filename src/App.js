2997cec933470ef9ca5d4edbef6a92e1ab3865b2
Jana Savchenko <yana.s.savchenko@gmail.com>
2020-04-14 22:34:06 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
 import Layout from './hoc/Layout/Layout'
 import Quiz from './containers/Quiz/Quiz'
-import './App.css';
+import Auth from './containers/Auth/Auth'; 
+import QuizList from './containers/QuizeList/QuizeList';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path='/auth' component={Auth}/>
+        <Route path='/quiz-creator' component={QuizCreator}/>
+        <Route path='/quiz/:id' component={Quiz}/>
+        <Route path='/' component={QuizList}/>
+      </Switch>
     </Layout>
   );
 }
