a76bcb3330cc0fa7e6cce7f8a8b7745d9f4f406c
Jana Savchenko <yana.s.savchenko@gmail.com>
2020-04-15 20:05:53 +0300

    Add content to Auth component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,56 @@
 import React from 'react';
 
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+
 export default class Auth extends React.Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = (e) => {
+    e.prevenDefault();
+  }
 
   render() {
     return (
-      <div>
-        <h1>Auth  </h1>
+      <div className={classes.auth}>
+        <div>
+          <h1>Authentication</h1>
+
+          <form
+            onSubmit={this.submitHandler}
+            className={classes.authForm}
+          >
+            <Input
+              label="Email"
+            />
+            <Input
+              label="Password"
+              errorMessage="test error"
+            />
+
+            <Button
+              type="success"
+              onclick={this.loginHandler}
+            >
+              Log in
+            </Button>
+
+            <Button
+              type="primary"
+              onclick={this.registerHandler}
+            >
+              Sign up
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
