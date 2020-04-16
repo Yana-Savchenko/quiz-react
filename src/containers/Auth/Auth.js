a79bf46399b099ce040b999bf5e265186f84e75a
Jana Savchenko <yana.s.savchenko@gmail.com>
2020-04-16 16:02:17 +0300

    Create auth form validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,36 @@ import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends React.Component {
 
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Email is invalid',
+        valid: false,
+        touched: false,
+        validators: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Password is invalid',
+        valid: false,
+        touched: false,
+        validators: {
+          required: true,
+          minLength: 6,
+        },
+      }
+    }
+  }
+
   loginHandler = () => {
 
   }
@@ -15,9 +45,68 @@ export default class Auth extends React.Component {
   }
 
   submitHandler = (e) => {
-    e.prevenDefault();
+    e.preventDefault();
   }
 
+  validateControl = (value, validators) => {
+    if (!validators) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validators.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (validators.email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      isValid = re.test(String(value).toLowerCase()) && isValid;
+    }
+
+    if (validators.minLength) {
+      isValid = value.length >= validators.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  onChangeHandler = (e, controlName) => {
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    control.value = e.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validators);
+
+    formControls[controlName] = control;
+
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid;
+    })
+
+    this.setState({ formControls, isFormValid })
+  }
+  renderInputs = () => {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          touched={control.touched}
+          valid={control.valid}
+          label={control.label}
+          shouldValidate={!!control.validators}
+          errorMessage={control.errorMessage}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
   render() {
     return (
       <div className={classes.auth}>
@@ -28,17 +117,12 @@ export default class Auth extends React.Component {
             onSubmit={this.submitHandler}
             className={classes.authForm}
           >
-            <Input
-              label="Email"
-            />
-            <Input
-              label="Password"
-              errorMessage="test error"
-            />
+            {this.renderInputs()}
 
             <Button
               type="success"
               onclick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Log in
             </Button>
@@ -46,6 +130,7 @@ export default class Auth extends React.Component {
             <Button
               type="primary"
               onclick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Sign up
             </Button>
